59ecf78df15a1bd0a5729f9c90711f3164efe9c8
sabitertan <sabitertan@gmail.com>
2022-01-30 12:39:39 -0500

    Added capture image from video source and decode under Video page

---
+++ BlazorWasmBarcodeScanZxingNet/Client/wwwroot/js/webcam.js
@@ -0,0 +1,28 @@
+﻿function startVideo(src) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            let video = document.getElementById(src);
+            if ("srcObject" in video) {
+                video.srcObject = stream;
+            } else {
+                video.src = window.URL.createObjectURL(stream);
+            }
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+            //mirror image
+            video.style.transform = "scaleX(-1)";
+        });
+    }
+}
+
+function getFrame(src, dest, dotNetHelper) {
+    let video = document.getElementById(src);
+    let canvas = document.getElementById(dest);
+    canvas.getContext('2d').drawImage(video, 0, 0, 1024, 1024);
+
+    let dataUrl = canvas.toDataURL("image/jpeg");
+    return dataUrl;
+    //return BINDING.js_string_to_mono_string(dataUrl);
+    //dotNetHelper.invokeMethodAsync('ProcessImage', dataUrl);
+}
